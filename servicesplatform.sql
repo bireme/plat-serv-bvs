581967440d1e12619ee6001c2dbb874bf85bf649
Wilson Moura <wilsonsmoura@gmail.com>
2016-08-04 17:46:58 -0300

    Fixed #4 -- Adjustments in database structure file

--- servicesplatform.sql
+++ servicesplatform.sql
@@ -1,149 +1,172 @@
-/*
-SQLyog Community Edition- MySQL GUI v8.12 
-MySQL - 5.0.67 : Database - servicesplatform
-*********************************************************************
-*/
+-- phpMyAdmin SQL Dump
+-- version 3.5.5
+-- http://www.phpmyadmin.net
+--
+-- Servidor: mysql.teste.bireme.br
+-- Tempo de Geração: 04/08/2016 às 15:35:28
+-- Versão do Servidor: 5.1.57
+-- Versão do PHP: 5.3.0
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`servicesplatform` /*!40100 DEFAULT CHARACTER SET latin1 */;
-
+--
+-- Banco de Dados: `servicesplatform`
+--
+CREATE DATABASE IF NOT EXISTS `servicesplatform` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 USE `servicesplatform`;
 
-/*Table structure for table `directories` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `directories`;
+--
+-- Estrutura da tabela `directories`
+--
 
-CREATE TABLE `directories` (
-  `dirID` bigint(10) unsigned NOT NULL auto_increment,
-  `name` char(20) character set utf8 NOT NULL,
-  `offline` int(10) NOT NULL default '0',
-  `userID` varchar(150) character set utf8 NOT NULL,
-  `public` int(10) default '0',
-  PRIMARY KEY  (`dirID`,`userID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `directories` (
+  `dirID` bigint(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` char(20) NOT NULL,
+  `offline` int(10) NOT NULL DEFAULT '0',
+  `sysUID` int(10) UNSIGNED NOT NULL,
+  `public` int(10) DEFAULT '0',
+  PRIMARY KEY (`dirID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
-/*Table structure for table `docTitle` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `docTitle`;
+--
+-- Estrutura da tabela `docTitle`
+--
 
-CREATE TABLE `docTitle` (
+CREATE TABLE IF NOT EXISTS `docTitle` (
   `lang` char(2) NOT NULL,
   `docTitle` varchar(1000) NOT NULL,
   `docID` varchar(100) NOT NULL,
-  PRIMARY KEY  (`lang`,`docID`)
+  PRIMARY KEY (`docID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-/*Table structure for table `documents` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `documents`;
+--
+-- Estrutura da tabela `documents`
+--
 
-CREATE TABLE `documents` (
-  `docID` varchar(100) character set utf8 NOT NULL,
-  `srcID` varchar(100) character set utf8 NOT NULL,
-  `authors` longtext character set utf8 NOT NULL,
-  `serial` varchar(200) character set utf8 NOT NULL,
-  `keywords` longtext character set utf8,
-  `year` varchar(4) character set utf8 default NULL,
-  `number` varchar(10) character set utf8 default NULL,
-  `volume` varchar(10) character set utf8 default NULL,
-  `suppl` varchar(50) character set utf8 default NULL,
-  `publication_date` varchar(22) character set utf8 default NULL,
-  `process_date` varchar(22) character set utf8 default NULL,
-  `docURL` varchar(1000) character set utf8 NOT NULL,
-  `title` varchar(500) default NULL,
-  PRIMARY KEY  (`docID`,`srcID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `documents` (
+  `docID` varchar(100) NOT NULL,
+  `srcID` varchar(100) NOT NULL,
+  `authors` longtext NOT NULL,
+  `serial` varchar(200) NOT NULL,
+  `keywords` longtext,
+  `year` varchar(4) DEFAULT NULL,
+  `number` varchar(10) DEFAULT NULL,
+  `volume` varchar(10) DEFAULT NULL,
+  `suppl` varchar(50) DEFAULT NULL,
+  `publication_date` varchar(22) DEFAULT NULL,
+  `process_date` varchar(22) DEFAULT NULL,
+  `docURL` varchar(1000) NOT NULL,
+  `title` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`docID`,`srcID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-/*Table structure for table `profiles` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `profiles`;
+--
+-- Estrutura da tabela `profiles`
+--
 
-CREATE TABLE `profiles` (
-  `profileID` bigint(10) unsigned NOT NULL auto_increment,
-  `userID` varchar(150) character set utf8 NOT NULL default '0',
-  `sysUID` int(10) unsigned default '0',
-  `profileText` varchar(150) character set utf8 NOT NULL default '',
-  `profileName` varchar(25) character set utf8 NOT NULL default '',
-  `profileStatus` char(3) character set utf8 NOT NULL default '',
-  `profileDefault` tinyint(1) NOT NULL default '0',
-  `creationDate` datetime NOT NULL default CURRENT_TIMESTAMP,
-  `id_grande_area` int(10) unsigned default '0',
-  `id_sub_area` int(10) unsigned default '0',
-  PRIMARY KEY  (`profileID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `profiles` (
+  `profileID` bigint(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `sysUID` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `profileText` varchar(150) NOT NULL DEFAULT '',
+  `profileName` varchar(25) NOT NULL DEFAULT '',
+  `profileStatus` char(3) NOT NULL DEFAULT '',
+  `creationDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id_grande_area` int(10) UNSIGNED DEFAULT '0',
+  `id_sub_area` int(10) UNSIGNED DEFAULT '0',
+  `profileDefault` tinyint(1) NOT NULL,
+  PRIMARY KEY (`profileID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
-/*Table structure for table `userLinks` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `userLinks`;
+--
+-- Estrutura da tabela `userLinks`
+--
 
-CREATE TABLE `userLinks` (
-  `linkID` bigint(10) unsigned NOT NULL auto_increment,
-  `url` varchar(200) character set utf8 NOT NULL default '',
-  `description` text character set utf8 NOT NULL,
-  `inHome` tinyint(1) NOT NULL default '0',
-  `name` varchar(150) character set utf8 NOT NULL default '',
-  `userID` varchar(150) character set utf8 NOT NULL default '0',
-  `rate` int(10) NOT NULL default '0',
-  PRIMARY KEY  (`linkID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `userLinks` (
+  `linkID` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
+  `url` varchar(300) NOT NULL,
+  `description` text NOT NULL,
+  `inHome` tinyint(1) NOT NULL DEFAULT '0',
+  `name` varchar(150) NOT NULL DEFAULT '',
+  `sysUID` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `rate` int(10) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`linkID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
-/*Table structure for table `userNews` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `userNews`;
+--
+-- Estrutura da tabela `userNews`
+--
 
-CREATE TABLE `userNews` (
-  `userID` varchar(150) NOT NULL default '0',
-  `url` varchar(150) NOT NULL default '',
-  `inHome` tinyint(1) NOT NULL default '0' COMMENT 'flag para a publicacao ou nao na home page',
-  `newsID` bigint(10) unsigned NOT NULL auto_increment,
+CREATE TABLE IF NOT EXISTS `userNews` (
+  `sysUID` int(150) UNSIGNED NOT NULL DEFAULT '0',
+  `url` varchar(300) NOT NULL,
+  `inHome` tinyint(1) NOT NULL DEFAULT '0',
+  `newsID` bigint(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(150) NOT NULL,
   `description` text NOT NULL,
-  `rate` int(10) NOT NULL default '1',
-  PRIMARY KEY  (`newsID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `rate` int(10) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`newsID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
-/*Table structure for table `userShelf` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `userShelf`;
+--
+-- Estrutura da tabela `userShelf`
+--
 
-CREATE TABLE `userShelf` (
-  `userDirID` bigint(10) unsigned NOT NULL auto_increment,
-  `userID` varchar(150) character set utf8 NOT NULL default '0',
-  `docID` varchar(30) character set utf8 NOT NULL default '',
-  `citedStat` tinyint(1) NOT NULL default '0',
-  `accessStat` tinyint(1) NOT NULL default '0',
-  `insertDate` datetime NOT NULL default CURRENT_TIMESTAMP,
-  `rate` int(10) NOT NULL default '0',
-  `visible` tinyint(1) unsigned NOT NULL default '0',
-  PRIMARY KEY  (`userDirID`,`docID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `userShelf` (
+  `userDirID` int(10) UNSIGNED NOT NULL,
+  `sysUID` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `docID` varchar(100) NOT NULL DEFAULT '',
+  `citedStat` tinyint(1) NOT NULL DEFAULT '0',
+  `accessStat` tinyint(1) NOT NULL DEFAULT '0',
+  `insertDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `rate` int(11) NOT NULL DEFAULT '0',
+  `visible` tinyint(1) NOT NULL DEFAULT '0',
+  `shelfID` bigint(10) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`shelfID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
-/*Table structure for table `users` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `users`;
+--
+-- Estrutura da tabela `users`
+--
 
-CREATE TABLE `users` (
-  `sysUID` bigint(10) unsigned NOT NULL auto_increment,
-  `userID` varchar(150) character set utf8 NOT NULL default '',
-  `userFirstName` varchar(150) character set utf8 NOT NULL default '',
-  `userLastName` varchar(150) character set utf8 NOT NULL default '',
-  `userEmail` varchar(45) character set utf8 NOT NULL default '',
-  `userPassword` varchar(32) character set utf8 NOT NULL default '',
-  `userGender` char(1) character set utf8 NOT NULL default '',
-  `userAffiliation` varchar(45) character set utf8 default '',
-  `userDegree` varchar(45) character set utf8 default '',
-  `userCountry` varchar(150) character set utf8 default '',
-  `userSource` varchar(150) character set utf8 default '',
-  `sguID` varchar(150) character set utf8 default '',
-  PRIMARY KEY  (`sysUID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `users` (
+  `userFirstName` varchar(150) NOT NULL DEFAULT '',
+  `userLastName` varchar(150) NOT NULL DEFAULT '',
+  `userEmail` varchar(45) NOT NULL DEFAULT '',
+  `userPassword` varchar(32) NOT NULL DEFAULT '',
+  `userGender` char(1) NOT NULL DEFAULT '',
+  `userAffiliation` varchar(45) DEFAULT '',
+  `userDegree` varchar(45) DEFAULT '',
+  `sysUID` bigint(10) NOT NULL AUTO_INCREMENT,
+  `userID` varchar(150) DEFAULT NULL,
+  `sguID` varchar(150) DEFAULT NULL,
+  `userCountry` varchar(150) DEFAULT NULL,
+  `userSource` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`sysUID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
