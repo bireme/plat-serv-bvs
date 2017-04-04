5a64ae527fdda27456441d7357e7caca2f815616
Wilson Moura <wilsonsmoura@gmail.com>
2017-04-04 09:42:56 -0300

    CSS adjustments

--- server/templates/default/sidebar.tpl.php
+++ server/templates/default/sidebar.tpl.php
@@ -50,12 +50,12 @@
         <!-- top navigation -->
         <div class="top_nav">
           <div class="nav_menu">
-            <nav class="" role="navigation">
+            <nav class="" role="navigation" style="display: flex;">
               <div class="nav toggle">
                 <a id="menu_toggle"><i class="fa fa-bars"></i></a>
               </div>
 
-              <form action="<?=VHL_SEARCH_PORTAL_DOMAIN?>/portal/" method="get" target="_blank">
+              <form action="<?=VHL_SEARCH_PORTAL_DOMAIN?>/portal/" method="get" target="_blank" style="width: 100%">
                 <div class="search_bar">
                   <div class="col-xs-11 form-group pull-right top_search">
                     <div class="input-group">
@@ -69,13 +69,13 @@
                 </div>
               </form>
 
-              <ul class="nav navbar-nav navbar-right">
-                <li class="">
-                  <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+              <ul class="nav navbar-nav navbar-right profile_menu">
+                <li>
+                  <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false" style="padding-bottom: 21px;">
                     <img src="<?=RELATIVE_PATH?>/images/<?=$_SESSION["skin"]?>/loggedin.png" alt=""><?=$_SESSION["userFirstName"]?>
                     <span class=" fa fa-angle-down"></span>
                   </a>
-                  <ul class="dropdown-menu dropdown-usermenu pull-right">
+                  <ul class="dropdown-menu dropdown-usermenu pull-right" style="width: 100%;">
                     <!--li><a href="javascript:;"><?=CHANGE_PASSWORD?></a></li-->
                     <li><a href="<?=RELATIVE_PATH?>/controller/logout/control/business"><i class="fa fa-sign-out pull-right"></i><?=LOGOFF?></a></li>
                   </ul>
