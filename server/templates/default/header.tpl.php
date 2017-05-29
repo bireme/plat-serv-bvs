d76455be06aebdfab108aa1de260e308d29d04a3
Wilson Moura <wilsonsmoura@gmail.com>
2017-05-29 14:41:44 -0300

    Fixed #78 -- Add Google Analytics code in configuration files

--- server/templates/default/header.tpl.php
+++ server/templates/default/header.tpl.php
@@ -35,6 +35,18 @@
     <script type="text/javascript">
         var LANG = "<?php echo $_SESSION['lang'] ? $_SESSION['lang'] : DEFAULT_LANG; ?>";
     </script>
+
+    <?php if ( !empty(GOOGLE_ANALYTICS) ) : ?>
+    <script type="text/javascript">
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','__gaTracker');
+
+        __gaTracker('create', '<?php echo GOOGLE_ANALYTICS; ?>', 'auto');
+        __gaTracker('send','pageview');
+    </script>
+    <?php endif; ?>
   </head>
   <body class="nav-md">
     <div class="container body">
